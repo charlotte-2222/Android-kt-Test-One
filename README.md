b5aae027f6cc1f396bab1f99f14a0a6303cfde37
charlotte-2222 <ayy.charlotte@gmail.com>
2022-06-14 10:24:35 -0400

    Initial commit - Test details unknown at present

---
+++ README.md
@@ -0,0 +1,10 @@
+# CPT-188 Test One
+## Charlotte Childers
+
+*This is was created ahead of time, the test contents are not currently known*
+
+### Test details:
+> Online, 6:00pm – 8:00pm,
+> Chapters 1 – 3
+> Hands on to create an App similar to chapters 1 - 3.
+___
---
+++ app/src/main/java/com/example/charlotte_childers_cpt_188_test_1/MainActivity.kt
@@ -0,0 +1,14 @@
+/*Charlotte Childers
+* CPT-188 Test One
+* 6-14-22*/
+package com.example.charlotte_childers_cpt_188_test_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
